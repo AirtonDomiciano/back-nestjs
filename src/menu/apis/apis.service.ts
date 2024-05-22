bfe96a0f42e9eb749934b0703f7cff3fb241cecd
PedroPetrikoski <pedro.ribeiro@xpert.com.br>
2024-05-22 15:06:13 -0300

    subindo listagem apis

--- src/menu/apis/apis.service.ts
+++ src/menu/apis/apis.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Apis } from './entities/apis.entity';
+import { ParamsSave } from './dto/apis-params.dto';
 
 @Injectable()
 export class ApisService {
@@ -10,31 +11,31 @@ export class ApisService {
     private apisRepository: Repository<Apis>,
   ) {}
 
-  async create(apis: Apis): Promise<Apis | string> {
-    let verificacao = true;
-    let mensagem = '';
+  async create(
+    apis: Apis,
+  ): Promise<ParamsSave> {
+    let msg = '';
 
-    if (apis.nome.length === 0 || apis.nome === null) {
-      verificacao = false;
-      mensagem += 'O campo Nome é obrigatório!';
+    if (!apis.nome) {
+      msg += 'O campo Nome é obrigatório!';
     }
 
-    if (apis.url.length === 0 || apis.url === null) {
-      verificacao = false;
-      mensagem += 'O campo URL é obrigatório!';
+    if (!apis.url) {
+      msg += 'O campo URL é obrigatório!';
     }
 
-    if (apis.rapidApiHost.length === 0 || apis.rapidApiHost === null) {
-      verificacao = false;
-      mensagem += 'O campo RapidApi-Host é obrigatório!';
+    if (!apis.rapidApiHost) {
+      msg += 'O campo RapidApi-Host é obrigatório!';
     }
 
-    if (verificacao) {
-      const updatedApis = await this.apisRepository.save(apis);
-      return updatedApis;
-    } else {
-      return mensagem;
+    const success = msg.length === 0;
+
+    if (!success) {
+      return { success, msg };
     }
+
+    const input = await this.apisRepository.save(apis);
+    return { input, success };
   }
 
   async findAll(): Promise<Apis[]> {
@@ -45,27 +46,27 @@ export class ApisService {
     return await this.apisRepository.findOne({ where: { idApis: id } });
   }
 
-  async update(id: number, apis: Apis): Promise<Apis | string> {
+  async update(id: number, api: Apis): Promise<Apis | string> {
     let verificacao = true;
     let mensagem = '';
 
-    if (apis.nome.length === 0 || apis.nome === null) {
+    if (!api.nome) {
       verificacao = false;
       mensagem += 'O campo Nome é obrigatório!';
     }
 
-    if (apis.url.length === 0 || apis.url === null) {
+    if (!api.url) {
       verificacao = false;
       mensagem += 'O campo URL é obrigatório!';
     }
 
-    if (apis.rapidApiHost.length === 0 || apis.rapidApiHost === null) {
+    if (!api.rapidApiHost) {
       verificacao = false;
       mensagem += 'O campo RapidApi-Host é obrigatório!';
     }
 
     if (verificacao) {
-      const updatedApis = await this.apisRepository.save(apis);
+      const updatedApis = await this.apisRepository.save(api);
       return updatedApis;
     } else {
       return mensagem;
@@ -74,7 +75,7 @@ export class ApisService {
 
   async remove(id: number): Promise<string> {
     await this.apisRepository.delete(id);
-    const mensagem = 'Usuário deletado!';
+    const mensagem = 'Api deletado!';
     return mensagem;
   }
 }
